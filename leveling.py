17d97a7b8402c34ce7df8704e8203b3977ffff87
Albert Wang <albert.wang.2004@gmail.com>
2024-12-13 23:32:53 -0500

    hi

--- leveling.py
+++ leveling.py
@@ -2,7 +2,7 @@ from scipy.optimize import minimize
 from scipy.optimize import LinearConstraint
 import numpy as np
 
-support = list(range(1, 11))
+support = list(range(1, 51))
 
 def evaluate(support, prev_dist, next_dist, alpha):
     score = 0
@@ -11,14 +11,15 @@ def evaluate(support, prev_dist, next_dist, alpha):
             score += next * j / (alpha * next + (1 - alpha) * prev)
     return score
 
-prev_dist = [0.10, 0.01, 0.03, 0.05, 0.06, 0.10, 0.15, 0.20, 0.12, 0.20]
-alpha = 0.5
+prev_dist = np.random.dirichlet(np.ones(len(support)), size=1)[0]
+alpha = 0.4
+print(prev_dist, alpha)
 
 def find_next_dist(support, prev_dist, alpha):
     def objective(next_dist):
         return -evaluate(support, prev_dist, next_dist, alpha)
 
-    initial_guess = [0.1] * len(prev_dist)
+    initial_guess = [1/len(support)] * len(prev_dist)
     bounds = [(0, 1)] * len(prev_dist)
 
     # Adding a linear constraint to ensure the sum of next_dist is 1
@@ -32,10 +33,14 @@ def combine_dist(support, prev_dist, next_dist, alpha):
 
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-for it in tqdm(range(4)):
+plt.plot(support, prev_dist, label=f'Iteration 0', marker='o')
+plt.xlabel('Support')
+plt.ylabel('Distribution')
+plt.legend()
+for it in tqdm(range(1,10)):
     next_dist = find_next_dist(support, prev_dist, alpha)
-    prev_dist = combine_dist(support, prev_dist, next_dist, alpha)
-
+    prev_dist = next_dist
+    
     plt.plot(support, prev_dist, label=f'Iteration {it}', marker='o')
     plt.xlabel('Support')
     plt.ylabel('Distribution')
